d3764a199c1d5eba1dc6e418ab6a1e686a006cde
motea927 <motea927@yahoo.com.tw>
2021-09-17 21:48:30 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>hello world</p>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
